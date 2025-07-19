8f77cc27bf2c25aa63e86f6f483db68540c50066
Mohannad Jaradat <jaradatm2@hotmail.com>
2025-07-19 16:18:42 +0300

    ASCII codes

---
+++ projects/Formatted Printing/ASCII codes.py
@@ -0,0 +1,9 @@
+# All characters are stored in the memory as numbers.
+# The ord() function returns the Unicode code point of a given character.
+print (ord("A"))  # Output: 65
+print (ord("1")) # Output: 49
+
+print("-" * 20)
+# The chr() function returns the character that represents a given Unicode code point.
+print (chr(65))  # Output: 'A'
+print (chr(49))  # Output: '1'
